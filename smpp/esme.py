daac274eb9696cb6fe35b542577a1a328484e19f
dmaclay <dmaclay@gmail.com>
2010-10-10 13:55:45 +0200

    major refactoring - dropping unecessarily indirect functions

--- smpp/esme.py
+++ smpp/esme.py
@@ -18,7 +18,7 @@ class ESME:
 
     def bind_SMSC(self):
         out_hex = re.sub(' ','','00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38 00 53 55 42 4D 49 54 31 00 00 01 01 00')
-        print out_hex
+        print '<<<< bind_SMSC out_hex >>>>', out_hex
         out_bin = binascii.a2b_hex(out_hex)
         self.conn.send(out_bin)
         length_bin = self.conn.recv(4)
@@ -27,8 +27,8 @@ class ESME:
         unpack_pdu(length_bin + rest_bin)
 
 
-#esme = ESME()
-#esme.connect_SMSC('localhost')
-#esme.bind_SMSC()
-#esme.disconnect_SMSC()
+esme = ESME()
+esme.connect_SMSC('localhost')
+esme.bind_SMSC()
+esme.disconnect_SMSC()
 
