6c6bcd7fd5e1bb6da6a9d8d9c4b739ec16f864e0
dmaclay <dmaclay@gmail.com>
2010-10-12 12:27:17 +0200

    |icleanup<esc>:wq

--- tests.py
+++ tests.py
@@ -15,16 +15,31 @@ test_bin = binascii.a2b_hex(re.sub(' ','','00 00 00 3C 00 00 00 04 00 00 00 00 0
 unpack_pdu(test_bin)
 
 test_bin = binascii.a2b_hex(re.sub(' ','','00000000 00000021 00000000 00000000 00 00 00 00 02 0101016500 026600 00 00 00 00 00 00 00 00 00 00 0000000000000000000000000000000000000000000000000000000000 00000000 00000000 0000'))
-unpack_pdu(test_bin)
+print unpack_pdu(test_bin)
 
-test_bin = binascii.a2b_hex(re.sub(' ','','00000000 80000021 00000000 00000000 00 02 01010000000000 01010000000000'))
-unpack_pdu(test_bin)
+test_bin = binascii.a2b_hex(re.sub(' ','','00000000 80000021 00000000 00000000 00 02 01016565650000000000 01016666660000000000'))
+print unpack_pdu(test_bin)
 
 pack_pdu('generic_nack','ESME_ROK',1,'00112233445566778899')
 pack_pdu()
-unpack_pdu(pack_pdu())
+print unpack_pdu(pack_pdu())
 
 
-j = {'status': 'ESME_ROK', 'body': {'optional_parameters':[{'tag':'payload_type', 'value':0}]}, 'length': 16, 'sequence': 0, 'command': 'bind_transmitter'}
+j = {
+    'header': {
+        'command_length': 16,
+        'command_id': 'bind_transmitter',
+        'command_status': 'ESME_ROK',
+        'sequence_number': 0
+    },
+    'body': {
+        'optional_parameters': [
+            {
+                'tag':'payload_type',
+                'value':0
+            }
+        ]
+    }
+}
 
 json_to_pdu(j)
