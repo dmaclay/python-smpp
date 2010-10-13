d0f216e3a4e3ed25e43d4d01bcf6b18b49da93ef
dmaclay <dmaclay@gmail.com>
2010-10-14 00:45:34 +0200

    can encode bind

--- smpp/esme.py
+++ smpp/esme.py
@@ -17,7 +17,7 @@ class ESME:
 
 
     def bind_SMSC(self):
-        out_hex = re.sub(' ','','00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38 00 53 55 42 4D 49 54 31 00 00 01 01 00')
+        out_hex = re.sub(r'\s','','''00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38 00 53 55 42 4D 49 54 31 00 00 01 01 00''')
         print '<<<< bind_SMSC out_hex >>>>', out_hex
         out_bin = binascii.a2b_hex(out_hex)
         self.conn.send(out_bin)
