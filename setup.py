4a0e656ba9b8606f10c7116987653403a6f412e0
Simon de Haan <simon@soocial.com>
2010-10-14 11:00:44 +0200

    Updating README and adding setup.py for easier installation into other projects using pip & friends.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "python-smpp",
+    version = "0.1",
+    url = 'http://github.com/smn/python-smpp',
+    license = 'BSD',
+    description = "Python SMPP Library",
+    author = 'David Maclay',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires = [],
+)
+
