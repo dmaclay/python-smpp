99858d87b3cc755ae1c366cd6da1ec8f44e9e767
Simon de Haan <simon@praekeltfoundation.org>
2011-03-31 09:08:23 +0200

    redoing how python-smpp is packaged

--- README.rst
+++ README.rst
@@ -1,14 +1,13 @@
 Python SMPP
 ===========
 
-An SMPP version 3.4 library written in Python, suitable for use in [Twisted][twisted]
+An SMPP version 3.4 library written in Python, suitable for use in Twisted_.
 
-To get started with development:
+To get started with development::
 
     $ virtualenv --no-site-packges ve/
     $ source ve/bin/activate
     (ve)$ pip install -r requirements.pip
-    (ve)$ python setup.py develop
     (ve)$ python
     >>> import smpp
     >>>
@@ -17,4 +16,4 @@ Run the tests with nose
 
     (ve)$ nosetests
 
-[twisted]: http://www.twistedmatrix.com
+.. _Twisted: http://www.twistedmatrix.com
--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup, find_packages
 
+def listify(filename):
+    return filter(None, open(filename, 'r').readlines())
+
 setup(
     name = "python-smpp",
     version = "0.1",
     url = 'http://github.com/dmaclay/python-smpp',
     license = 'BSD',
     description = "Python SMPP Library",
+    long_description = open('README.rst','r').read(),
     author = 'David Maclay',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    install_requires = [],
+    author_email = 'dev@praekeltfoundation.org',
+    packages = find_packages(),
+    install_requires = ['setuptools'].extend(listify('requirements.pip')),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
 
