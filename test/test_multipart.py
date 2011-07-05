0867959d66ec0277166fe5cd6c92363cc9afefa5
dmaclay <dmaclay@gmail.com>
2011-07-05 10:49:20 +0200

    testing

--- test/test_multipart.py
+++ test/test_multipart.py
@@ -1,4 +1,4 @@
-import operator
+
 import unittest, collections
 
 from smpp.pdu_builder import *
@@ -31,26 +31,28 @@ class MultipartTestCase(unittest.TestCase):
         self.assertEquals(detect_multipart(unpack_pdu(csm16.get_bin()))['multipart_type'], 'CSM16')
 
 
-    def all(self):
+    def test_ordering(self):
+        """
+        Out of order pieces must be re-assembled in-order
+        """
         sar_1 = DeliverSM(1, short_message='\x00\x03\xff\x04\x01There she was just a')
         sar_2 = DeliverSM(1, short_message='\x00\x03\xff\x04\x02 walking down the street,')
         sar_3 = DeliverSM(1, short_message='\x00\x03\xff\x04\x03 singing doo wa diddy')
         sar_4 = DeliverSM(1, short_message='\x00\x03\xff\x04\x04 diddy dum diddy do')
 
-        mess_list = []
-        mess_list.append(detect_multipart(unpack_pdu(sar_3.get_bin())))
-        mess_list.append(detect_multipart(unpack_pdu(sar_4.get_bin())))
-        mess_list.append(detect_multipart(unpack_pdu(sar_2.get_bin())))
-        mess_list.append(detect_multipart(unpack_pdu(sar_1.get_bin())))
-
-        print mess_list
-        print ''.join([i['part_message'] for i in mess_list])
-        print '\n'
-        mess_list.sort(key=operator.itemgetter('part_number'))
-        print mess_list
-        print ''.join([i['part_message'] for i in mess_list])
+        multi = MultipartMessage()
+        multi.add_pdu(sar_3.get_obj())
+        multi.add_pdu(sar_4.get_obj())
+        multi.add_pdu(sar_2.get_obj())
+        multi.add_pdu(sar_1.get_obj())
+        self.assertEquals(multi.get_completed()['message'],
+                'There she was just a walking down the street, singing doo wa diddy diddy dum diddy do')
 
 
+    def test_real_csm_data(self):
+        """
+        Test with real-world data which uses the CSM format.
+        """
 
         asif_1 = {'body': {'mandatory_parameters': {'priority_flag': 0, 'source_addr': '261xxx720371', 'protocol_id': 0, 'replace_if_present_flag': 0, 'registered_delivery': 0, 'dest_addr_ton': 'international', 'source_addr_npi': 'ISDN', 'schedule_delivery_time': '', 'dest_addr_npi': 'ISDN', 'sm_length': 159, 'esm_class': 64, 'data_coding': 0, 'service_type': '', 'source_addr_ton': 'international', 'sm_default_msg_id': 0, 'validity_period': '', 'destination_addr': '261xxx782943', 'short_message': '\x05\x00\x03\x1a\x02\x01I try to send sms testing vumi sms sms sms sms msm sms sms sms sms sms sms sms sms sms ssms sms smS sms sms sms sms sms sms sms sns sns sms sms sms sms s'}, 'optional_parameters': [{'length': 2, 'tag': 'user_message_reference', 'value': 91}, {'length': 16, 'tag': 'dest_subaddress', 'value': 'a0000410020601030303070802090403'}]}, 'header': {'command_status': 'ESME_ROK', 'command_length': 242, 'sequence_number': 23, 'command_id': 'deliver_sm'}}
 
