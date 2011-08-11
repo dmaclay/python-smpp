6586535e3180d9b021dfcb39abb6d2da6fd812ee
dmaclay <dmaclay@gmail.com>
2011-08-11 13:51:17 +0200

    && \

--- run-tests.sh
+++ run-tests.sh
@@ -3,5 +3,5 @@ virtualenv --no-site-packages ve && \
 source ve/bin/activate && \
 nosetests --with-doctest --with-coverage --cover-package=smpp --with-xunit && \
 coverage xml smpp/* && \
-pep8 --repeat --exclude '0*.py' smpp > pep8.txt
+pep8 --repeat --exclude '0*.py' smpp > pep8.txt && \
 deactivate
