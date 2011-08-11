ac9bb6f064503935d8dc610575d3f9baef929c38
dmaclay <dmaclay@gmail.com>
2011-08-11 14:16:40 +0200

    deactivate & reactivate ve

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,8 @@
 virtualenv --no-site-packages ve && \
 source ve/bin/activate && \
 pip install -r requirements.pip && \
+deactivate
+source ve/bin/activate && \
 nosetests --with-doctest --with-coverage --cover-package=smpp --with-xunit && \
 coverage xml smpp/* && \
 pep8 --repeat --exclude '0*.py' smpp > pep8.txt && \
